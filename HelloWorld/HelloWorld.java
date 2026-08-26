6e96331f15fe170a64cb2fe36c2dd45e89652d35
christianrothfus-drake <christian.rothfus@drake.edu>
2026-08-26 15:25:35 -0500

    Creating my first simple console program in Java!

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,8 +2,27 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Count is: " + i);
+        for (int row = 1; row <= 5; row++)
+            {
+            int prior_numbers = triangle_sum(row - 1);
+            for (int col = 1; col <= (5 - row + 1); col++)
+            {
+                System.out.print((prior_numbers + col) + " ");
+            }
+            System.out.println();
         }
     }
+
+    public static int triangle_sum(int total_rows)
+    {
+        int current_sum = 0;
+
+        for (int row = 1; row <= total_rows; row++)
+            {
+            current_sum += (5 - row + 1);
+            }
+        
+        return current_sum;
+    }
+    
 }
