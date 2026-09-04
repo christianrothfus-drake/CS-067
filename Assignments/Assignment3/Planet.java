185d149b62c49c649778cfcb75a26bfef2c290b9
christianrothfus <christianrothfus1@gmail.com>
2026-09-03 23:55:09 -0500

    Uploading assignment 3

---
+++ Assignments/Assignment3/Planet.java
@@ -0,0 +1,50 @@
+package Assignment3;
+import java.lang.Math;
+
+public class Planet
+{
+    String name;
+    double orbitDistance;
+    double orbitCircumference;
+    double orbitSpeed;
+    double orbitProgress;
+
+    public Planet(String name, double orbitDistance, double orbitSpeed)
+    {
+        this.name = name;
+        this.orbitDistance = orbitDistance;
+        this.orbitCircumference = Math.PI * orbitDistance * 2;
+        this.orbitSpeed = orbitSpeed;
+        this.orbitProgress = 0;
+    }
+
+    public void introduce()
+    {
+        System.out.println("Hello, I am " + name + "!");
+        System.out.println("I am orbiting at a distance of " + orbitDistance);
+        System.out.println("I am orbiting at a speed of " + orbitSpeed);
+
+        double orbitTimeSteps = orbitCircumference / orbitSpeed;
+
+        System.out.println("Consequently, it takes " + orbitTimeSteps + " time steps to complete my orbit.");
+    }
+
+    public void orbit()
+    {
+        int orbitsCompleted = 0;
+
+        for (int timeStep=1; timeStep<=1_000_000; timeStep++)
+        {
+            orbitProgress = orbitProgress + (orbitSpeed / orbitCircumference);
+
+            if (orbitProgress > 1)
+            {
+                orbitsCompleted++;
+                
+                System.out.println("I completed orbit " + orbitsCompleted + " on time step " + timeStep);
+
+                orbitProgress = orbitProgress - 1;
+            }
+        }
+    }
+}
---
+++ Assignments/Assignment3/PlanetDriver.java
@@ -0,0 +1,20 @@
+package Assignment3;
+
+// Acknowledgements
+//    Earth Wiki - Source for how fast the Earth travels around the sun
+//        https://en.wikipedia.org/wiki/Earth#Orbit_and_location
+
+public class PlanetDriver {
+    public static void main(String[] args)
+    {
+        String planetName = "Earth";
+        double orbitDistance = 93_000_000; // Approximate distance from the sun in millions of miles
+        double orbitSpeed = 66_622; // Approximate number of miles Earth travels in 1 hour
+
+        Planet myPlanet = new Planet(planetName, orbitDistance, orbitSpeed);
+
+        myPlanet.introduce();
+
+        myPlanet.orbit();
+    }
+}
